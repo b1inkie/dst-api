f1e3979a2ec6982cd4fd0eced63adfddba9d7df0
b1inkie <zinsserlan@126.com>
2024-09-08 22:24:21 +0800

    add scripts/entityscript.lua

---
+++ scripts/entityscript.lua
@@ -0,0 +1,861 @@
+return {
+
+	["StackableSkinHack"] = {
+		params = {
+			{param = "target", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["ForceOutOfLimbo"] = {
+		params = {
+			{param = "state", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddInherentAction"] = {
+		params = {
+			{param = "act", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveInherentAction"] = {
+		params = {
+			{param = "act", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StartUpdatingComponent"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+			{param = "do_static_update", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StopUpdatingComponent"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StopUpdatingComponent_Deferred"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StartWallUpdatingComponent"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StopWallUpdatingComponent"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetComponentName"] = {
+		params = {
+			{param = "cmp", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddTag"] = {
+		params = {
+			{param = "tag", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveTag"] = {
+		params = {
+			{param = "tag", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddOrRemoveTag"] = {
+		params = {
+			{param = "tag", explain = "", type = ""},
+			{param = "condition", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["HasTag"] = {
+		params = {
+			{param = "tag", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["HasTags"] = {
+		params = {
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["HasOneOfTags"] = {
+		params = {
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddComponent"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveComponent"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetPrefabName"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetPrefabNameOverride"] = {
+		params = {
+			{param = "nameoverride", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetDeployExtraSpacing"] = {
+		params = {
+			{param = "spacing", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetDeploySmartRadius"] = {
+		params = {
+			{param = "radius", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetTerraformExtraSpacing"] = {
+		params = {
+			{param = "spacing", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetGroundTargetBlockerRadius"] = {
+		params = {
+			{param = "radius", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SpawnChild"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveChild"] = {
+		params = {
+			{param = "child", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddChild"] = {
+		params = {
+			{param = "child", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemovePlatformFollower"] = {
+		params = {
+			{param = "child", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddPlatformFollower"] = {
+		params = {
+			{param = "child", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StartThread"] = {
+		params = {
+			{param = "fn", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RunScript"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetBrain"] = {
+		params = {
+			{param = "brainfn", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetStateGraph"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["ListenForEvent"] = {
+		params = {
+			{param = "event", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "source", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveEventCallback"] = {
+		params = {
+			{param = "event", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "source", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["WatchWorldState"] = {
+		params = {
+			{param = "var", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["StopWatchingWorldState"] = {
+		params = {
+			{param = "var", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["PushEvent"] = {
+		params = {
+			{param = "event", explain = "", type = ""},
+			{param = "data", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetPhysicsRadiusOverride"] = {
+		params = {
+			{param = "radius", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetPhysicsRadius"] = {
+		params = {
+			{param = "default", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetAngleToPoint"] = {
+		params = {
+			{param = "x", explain = "", type = ""},
+			{param = "y", explain = "", type = ""},
+			{param = "z", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetPositionAdjacentTo"] = {
+		params = {
+			{param = "target", explain = "", type = ""},
+			{param = "distance", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["ForceFacePoint"] = {
+		params = {
+			{param = "x", explain = "", type = ""},
+			{param = "y", explain = "", type = ""},
+			{param = "z", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["FacePoint"] = {
+		params = {
+			{param = "x", explain = "", type = ""},
+			{param = "y", explain = "", type = ""},
+			{param = "z", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetDistanceSqToInst"] = {
+		params = {
+			{param = "inst", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["IsNear"] = {
+		params = {
+			{param = "otherinst", explain = "", type = ""},
+			{param = "dist", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetDistanceSqToPoint"] = {
+		params = {
+			{param = "x", explain = "", type = ""},
+			{param = "y", explain = "", type = ""},
+			{param = "z", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["IsNearPlayer"] = {
+		params = {
+			{param = "range", explain = "", type = ""},
+			{param = "isalive", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetNearestPlayer"] = {
+		params = {
+			{param = "isalive", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetDistanceSqToClosestPlayer"] = {
+		params = {
+			{param = "isalive", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["FaceAwayFromPoint"] = {
+		params = {
+			{param = "dest", explain = "", type = ""},
+			{param = "force", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["DoStaticPeriodicTask"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "initialdelay", explain = "", type = ""},
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["DoStaticTaskInTime"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["DoPeriodicTask"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "initialdelay", explain = "", type = ""},
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["DoTaskInTime"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["PushEventInTime"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "eventname", explain = "", type = ""},
+			{param = "data", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetTaskInfo"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["TimeRemainingInTask"] = {
+		params = {
+			{param = "taskinfo", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["ResumeTask"] = {
+		params = {
+			{param = "time", explain = "", type = ""},
+			{param = "fn", explain = "", type = ""},
+			{param = "...", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["PreviewBufferedAction"] = {
+		params = {
+			{param = "bufferedaction", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["PushBufferedAction"] = {
+		params = {
+			{param = "bufferedaction", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["OnBuilt"] = {
+		params = {
+			{param = "builder", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["CanInteractWith"] = {
+		params = {
+			{param = "inst", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["OnUsedAsItem"] = {
+		params = {
+			{param = "action", explain = "", type = ""},
+			{param = "doer", explain = "", type = ""},
+			{param = "target", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["CanDoAction"] = {
+		params = {
+			{param = "action", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["IsOnPassablePoint"] = {
+		params = {
+			{param = "include_water", explain = "", type = ""},
+			{param = "floating_platforms_are_not_passable", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["IsOnOcean"] = {
+		params = {
+			{param = "allow_boats", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["PutBackOnGround"] = {
+		params = {
+			{param = "radius", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["LoadPostPass"] = {
+		params = {
+			{param = "newents", explain = "", type = ""},
+			{param = "savedata", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetPersistData"] = {
+		params = {
+			{param = "data", explain = "", type = ""},
+			{param = "newents", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetInherentSceneAction"] = {
+		params = {
+			{param = "action", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetInherentSceneAltAction"] = {
+		params = {
+			{param = "action", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["LongUpdate"] = {
+		params = {
+			{param = "dt", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetClientSideInventoryImageOverride"] = {
+		params = {
+			{param = "flagname", explain = "", type = ""},
+			{param = "srcinventoryimage", explain = "", type = ""},
+			{param = "destinventoryimage", explain = "", type = ""},
+			{param = "destatlas", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetClientSideInventoryImageOverride"] = {
+		params = {
+			{param = "imagenamehash", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["SetClientSideInventoryImageOverrideFlag"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+			{param = "value", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["IsLightGreaterThan"] = {
+		params = {
+			{param = "lightThresh", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["HasDebuff"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["GetDebuff"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["AddDebuff"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+			{param = "prefab", explain = "", type = ""},
+			{param = "data", explain = "", type = ""},
+			{param = "skip_test", explain = "", type = ""},
+			{param = "pre_buff_fn", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+	["RemoveDebuff"] = {
+		params = {
+			{param = "name", explain = "", type = ""},
+
+		},
+		returns = {},
+		tips = "",
+		author = "",
+	},      
+
+}
