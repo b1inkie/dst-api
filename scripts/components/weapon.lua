d899a4e23be55b500412bbb8eb8d8b0555aa65c3
Lunarsolar <lunarsolar6174@126.com>
2024-09-08 10:18:56 +0800

    Update weapon.lua

--- scripts/components/weapon.lua
+++ scripts/components/weapon.lua
@@ -2,57 +2,70 @@ data = {
 
     ["OnRemoveFromEntity"] = {
         params = {
-
+            nil,
         },
         returns = {
-            
+            nil,  
         },
-        tips = "",
-        author = "",
+        tips = "移除weapon标签, 恢复装备者攻击范围",
+        author = "Runar",
     },
         
     ["SetDamage"] = {
         params = {
             {
                 param = "dmg", explain = "伤害值或伤害函数", type = "number|function",
-                params = {
+                fn_params = {
                     {param = "inst", explain = "武器的inst", type = "table"},
                     {param = "attacker", explain = "攻击者的inst", type = "table"},
                     {param = "target", explain = "攻击目标的inst", type = "table"},
                 },
+                fn_returns = {
+                    {explain = "伤害值", type = "number"},
+                }
             },
 
         },
         returns = {
             nil,
         },
-        tips = "如果dmg为函数型参数，show me等信息模组无法显示正确的攻击力",
+        tips = "设置武器伤害值。如果dmg为函数型参数，show me等信息模组无法显示正确的攻击力",
         author = "Runar",
     },
         
     ["SetRange"] = {
         params = {
-            {param = "attack", explain = "", type = ""},
-            {param = "hit", explain = "", type = ""},
+            {param = "attack", explain = "攻击预备距离", type = "number"},
+            {param = "hit", explain = "击中距离", type = "number|nil"},
 
         },
         returns = {
-            
+            nil,
         },
-        tips = "",
-        author = "",
+        tips = "设置武器攻击距离。若hit为nil，则hit = attack",
+        author = "Runar",
     },
         
     ["SetOnAttack"] = {
         params = {
-            {param = "fn", explain = "", type = ""},
+            {
+                param = "fn", explain = "击中后执行的函数", type = "function",
+                fn_params = {
+                    {param = "inst", explain = "武器的inst", type = "table"},
+                    {param = "attacker", explain = "攻击者的inst", type = "table"},
+                    {param = "target", explain = "攻击目标的inst", type = "table"},
+                },
+                fn_returns = {
+                    nil,
+                }
+            },
 
         },
         returns = {
-            
+            nil,
         },
-        tips = "",
-        author = "",
+        tips = "设置武器击中回调函数, 同SetAttackCallback",
+        author = "Runar",
     },
         
     ["SetOnProjectileLaunch"] = {
@@ -141,27 +154,38 @@ data = {
         
     ["SetAttackCallback"] = {
         params = {
-            {param = "fn", explain = "", type = ""},
+            {
+                param = "fn", explain = "击中后执行的函数", type = "function",
+                fn_params = {
+                    {param = "inst", explain = "武器的inst", type = "table"},
+                    {param = "attacker", explain = "攻击者的inst", type = "table"},
+                    {param = "target", explain = "攻击目标的inst", type = "table"},
+                },
+                fn_returns = {
+                    nil,
+                }
+            },
 
         },
         returns = {
-            
+            nil,
         },
-        tips = "",
-        author = "",
+        tips = "设置武器击中回调函数",
+        author = "Runar",
     },
         
     ["GetDamage"] = {
         params = {
-            {param = "attacker", explain = "", type = ""},
-            {param = "target", explain = "", type = ""},
+            {param = "attacker", explain = "攻击者的inst", type = "table"},
+            {param = "target", explain = "攻击目标的inst", type = "table"},
 
         },
         returns = {
-            
+            {explain = "通常伤害", type = "number"},
+            {explain = "特殊伤害", type = "number"},
         },
-        tips = "",
-        author = "",
+        tips = "获取攻击者装备此武器时对目标的伤害",
+        author = "Runar",
     },
         
     ["OnAttack"] = {
