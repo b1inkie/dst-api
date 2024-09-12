5263dce3f1216c420641798c96740d5be34f1e7c
Lunarsolar <lunarsolar6174@126.com>
2024-09-12 12:21:06 +0800

    Update preserver.lua

--- scripts/components/preserver.lua
+++ scripts/components/preserver.lua
@@ -2,26 +2,35 @@ data = {
 
     ["SetPerishRateMultiplier"] = {
         params = {
-            {param = "rate", explain = "", type = ""},
+            {
+                param = "rate", explain = "腐败率乘函系数", type = "function|number",
+                fn_params = {
+                    {param = "inst", explain = "容器inst", type = "entity"},
+                    {param = "item", explain = "物品inst", type = "entity"},
+                },
+                fn_returns = {
+                    {explain = "腐败率乘系数", type = "number"}
+                }
+            },
 
         },
         returns = {
             
         },
-        tips = "",
-        author = "",
+        tips = "设置腐败率乘函系数",
+        author = "Runar",
     },
         
     ["GetPerishRateMultiplier"] = {
         params = {
-            {param = "item", explain = "", type = ""},
+            {param = "item", explain = "物品inst", type = "entity"},
 
         },
         returns = {
-            
+            {explain = "腐败率乘系数", type = "number"}
         },
-        tips = "",
-        author = "",
+        tips = "获取腐败率乘系数",
+        author = "Runar",
     },
         
 }
